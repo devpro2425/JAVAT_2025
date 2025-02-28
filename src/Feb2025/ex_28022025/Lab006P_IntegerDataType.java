28758d9793149ea66eb0959f220bccd56d7aa367
Mehul <mehulpagi005@outlook.com>
2025-02-28 07:47:11 +0530

    Lab006 add

---
+++ src/Feb2025/ex_28022025/Lab006P_IntegerDataType.java
@@ -0,0 +1,13 @@
+package Feb2025.ex_28022025;
+
+public class Lab006P_IntegerDataType {
+    public static void main(String[] args) {
+        //Define the integer data type
+        //its look like data type= age
+        //variable name =age
+        // variable values =65
+        int age= 65;
+        System.out.println(age);
+    }
+
+}
