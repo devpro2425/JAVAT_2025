c8d00407f27589e06b980acd239fa9f6f44a0f6a
Mehul <mehulpagi005@outlook.com>
2025-03-07 07:35:45 +0530

    Lab009 short data type upload

--- src/March2025/ex_06032025/Lab008_Boolean_datatype.java
+++ src/March2025/ex_06032025/Lab008_Boolean_datatype.java
@@ -2,6 +2,7 @@ package March2025.ex_06032025;
 
 public class Lab008_Boolean_datatype {
     public static void main(String[] args) {
+        //boolean haave only two values yes and no
         boolean is_male=true;
         boolean is_female=false;
         boolean indian=true;
---
+++ src/March2025/ex_06032025/Lab009_Intergral_data_type.java
@@ -0,0 +1,20 @@
+package March2025.ex_06032025;
+
+public class Lab009_Intergral_data_type {
+    public static void main(String[] args) {
+        //byte can store only (-127) to 127
+        byte b =127;
+        //how they are stored in memory
+        //short can store from (-32768) to 32767
+        short a=-32768;
+        short c=0;
+        short d=-32766;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+
+    }
+}
