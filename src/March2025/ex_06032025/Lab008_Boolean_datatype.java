4f4aac1f0cfceacef393d24cc30cd0daad5aab82
Mehul <mehulpagi005@outlook.com>
2025-03-07 07:25:18 +0530

    Lab008 boolean data type upload

---
+++ src/March2025/ex_06032025/Lab008_Boolean_datatype.java
@@ -0,0 +1,11 @@
+package March2025.ex_06032025;
+
+public class Lab008_Boolean_datatype {
+    public static void main(String[] args) {
+        boolean is_male=true;
+        boolean is_female=false;
+        boolean indian=true;
+        boolean long_name_can_be_taken=true;
+        //is space allowed in boolean = no
+    }
+}
